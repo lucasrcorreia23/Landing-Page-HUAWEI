e27968bb8fe5fbc4cbb318a057408982e32d46ed
Lucas Rodrigues Correia <lucas_rc15@live.com>
2023-08-24 10:31:18 -0300

    Search Console

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -8,6 +8,7 @@ export default function Head() {
       Access Points Landing Page
     </title>
     <meta charSet="UTF-8" />
+    <meta name="google-site-verification" content="c-CD8g4f-dRvaNezvQnHmFwsEBD1B5a3ousshhbnRn8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="../dist/output.css" rel="stylesheet" />
         <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/a213531c-b59a-4368-8107-aadc01389900-loader.js" ></script>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,35 +3,30 @@ import { HomeIcon, Search, Contact2 } from "lucide-react"
 
 export function Menu() {
     return (
-        
+
         <div className="mx-auto flex items-center px-4 h-24 md:h-36 border-b border-zinc-350 border-gray-300  bg-emerald-50">
-          
 
-         
-          <nav className=" items-center flex-row flex w-full md:mx-auto">
-              <div className="w-8/12 md:flex md:justify-center md:w-full md:mx-auto">
-                  <a href="#" className="flex w-24 md:w-36 md:justify-center">
-                  
-                  <Image src="/prime8.svg" width={400} height={0} alt='Logotipo' />
-                  </a>
-              </div>
-          
-          
-              
 
-              
-              
-              <div className="flex-row group flex items-center justify-center  w-full">
-                <a href="tel:5548999247580" className=" px-8 whitespace-nowrap md:py-5 md:text-lg mx-auto font-black p-4 text-xs text-center bg-brand  rounded-lg items-center text-white hover:scale-105 transition-1s duration-200 flex flex-row ">
-                
-                
-                <b>Falar com Especialista</b>
-                <Image src="/headset.svg" width={32} height={0} alt='Headset' className="pl-2" />
-                </a>
+
+            <nav className=" items-center flex-row flex w-full md:mx-auto">
+                <div className="w-8/12 md:flex md:justify-center md:w-full md:mx-auto">
+                    <a href="#" className="flex w-24 md:w-36 md:justify-center">
+
+                        <Image src="/prime8.svg" width={400} height={0} alt='Logotipo' />
+                    </a>
                 </div>
-          </nav>
-          
-        
-      </div>
+
+                <div className="flex-row group flex items-center justify-center  w-full">
+                    <a href="tel:5548999247580" className=" px-8 whitespace-nowrap md:py-5 md:text-lg mx-auto font-black p-4 text-xs text-center bg-brand  rounded-lg items-center text-white hover:scale-105 transition-1s duration-200 flex flex-row ">
+
+
+                        <b>Falar com Especialista</b>
+                        <Image src="/headset.svg" width={32} height={0} alt='Headset' className="pl-2" />
+                    </a>
+                </div>
+            </nav>
+
+
+        </div>
     )
 }
